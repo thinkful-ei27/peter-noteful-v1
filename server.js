71c2a56580990a3a859bbbab0b4926f8d37c9f6c
Peter Ramos <peterdramos02@gmail.com>
2018-12-17 15:00:58 -0500

    build express server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 'use strict';
 
 // Load array of notes
+const express = require('express');
 const data = require('./db/notes');
+const app = express(); 
 
-console.log('Hello Noteful!');
-
-// INSERT EXPRESS APP CODE HERE...
+// ADD STATIC SERVER....
+app.listen(8080, function() {
+  console.info(`Server listening on ${this.address().port}`);
+}).on('error', err => console.log(err));
