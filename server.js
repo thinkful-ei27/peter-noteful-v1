9711f8fb20eb15f9975cc6bf0efb41da23ba7ef7
Peter Ramos <peterdramos02@gmail.com>
2018-12-17 15:28:31 -0500

    add GET /api/notes and /api/notes/:id

--- server.js
+++ server.js
@@ -9,3 +9,13 @@ const app = express();
 app.listen(8080, function() {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => console.log(err));
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  const note = data.find(item => item.id === Number(id));
+  res.json(note);
+});
