491f0b38f74c704c8f203df52606f6b90286d982
Peter Ramos <peterdramos02@gmail.com>
2018-12-19 14:43:28 -0500

    add module for /api routes

--- server.js
+++ server.js
@@ -2,11 +2,9 @@
 
 // Load array of notes
 const express = require('express');
-const data = require('./db/notes');
-const simDB = require('./db/simDB');
-const notes = simDB.initialize(data);
 const { PORT } = require('./config');
 const morgan = require('morgan');
+const notesRouter = require('./router/notes.router');
 const app = express(); 
 
 // Middleware
@@ -21,57 +19,7 @@ app.use(express.json());
 
 
 // Routes
-app.get('/api/notes', (req, res, next) => {
-  const { searchTerm } = req.query;
-
-  notes.filter(searchTerm, (err, list) => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(list); // responds with filtered array
-  });
-});
-
-app.get('/api/notes/:id', (req, res, next) => {
-  const { id } = req.params;
-
-  notes.find(id, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
-
-app.put('/api/notes/:id', (req, res, next) => {
-  const id = req.params.id;
-
-  const updateObj = {};
-  const updateFields = ['title', 'content'];
-
-  updateFields.forEach(field => {
-    if (field in req.body) {
-      updateObj[field] = req.body[field];
-    }
-  });
-
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
-});
-
-
+app.use('/api', notesRouter);
 
 // Error handling
 app.use(function (req, res, next) {
