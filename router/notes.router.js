c82c4d0c433d67287dbf30ae52abf21a40466637
Peter Ramos <peterdramos02@gmail.com>
2018-12-20 15:06:08 -0500

    add promisify-ed versions to notes.router.js

--- router/notes.router.js
+++ router/notes.router.js
@@ -8,27 +8,25 @@ const router = express.Router();
 router.get('/notes', (req, res, next) => {
   const { searchTerm } = req.query;
 
-  notes.filter(searchTerm, (err, list) => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(list); // responds with filtered array
-  });
+  notes.filter(searchTerm)
+    .then(list => res.json(list))
+    .catch(err => next(err));
 });
 
 router.get('/notes/:id', (req, res, next) => {
   const { id } = req.params;
 
-  notes.find(id, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
+  notes.find(id)
+    .then(item => {
+      if (item) {
+        res.json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 router.put('/notes/:id', (req, res, next) => {
@@ -43,16 +41,15 @@ router.put('/notes/:id', (req, res, next) => {
     }
   });
 
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
+  notes.update(id, updateObj)
+    .then(item => {
+      if (item) {
+        res.json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err => next(err));
 });
 
 router.post('/notes', (req, res, next) => {
@@ -65,34 +62,30 @@ router.post('/notes', (req, res, next) => {
     return next(err);
   }
 
-  notes.create(newItem, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.location(`http://${req.headers.host}/api/notes/${item.id}`).status(201).json(item);
-    } else {
-      next();
-    }
-  });
+  notes.create(newItem)
+    .then(item => {
+      if (item) {
+        res.location(`http://${req.headers.host}/api/notes/${item.id}`).status(201).json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err => next(err));
 });
 
 router.delete('/notes/:id', (req, res, next) => {
   const { id } = req.params;
   // How do i make sure that id from params matches id from notes?
 
-  notes.delete(id, (err, num) => {
-    if (num === null) {
-      const err = new Error('Item does not exist');
-      err.status = 500;
-      return next(err);
-    }
-    if (err) {
-      return next(err);
-    }
-    console.log(`Deleted note item \`${id}\``);
-    res.status(204).end();
-  });
+  notes.delete(id)
+    .then(item => {
+      if (item) {
+        res.status(204).end();
+      } else {
+        next();
+      }
+    })
+    .catch(err => next(err));
 });
 
 
